81a65f050f5d2df871a7aca5b8d9a3bb1ca18bff
Mert <mertturkmenoglu99@gmail.com>
2019-04-11 18:03:28 +0300

    Add Partial Function Example

---
+++ examples/partial_functions.py
@@ -0,0 +1,15 @@
+from functools import partial
+
+# Looks like a normal function definition
+def function(arg1, arg2, arg3):
+    return arg1 + arg2 + arg3
+
+
+# Function assignment
+# We leave the first parameter it will be 
+# passed on actual function call
+f = partial(function, 2, 3)
+
+# Actual function call
+# Expected output: 6
+print(f(1))
