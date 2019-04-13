15f6de9b3d4f6f7d4b9fd13c5286ce33a67f917d
Mert <mertturkmenoglu99@gmail.com>
2019-04-14 01:51:35 +0300

    Add Exception Handling Example

---
+++ examples/exception_handling.py
@@ -0,0 +1,13 @@
+# Exception handling program
+
+numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+try:
+    for i in range(len(numbers)):
+        print(numbers[i])
+  
+    # Throws error
+    print("11th element = %d" %(numbers[10]))
+  
+except IndexError: 
+    print("Index Error. Check your index!!!")
