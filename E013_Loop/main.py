9c2f26a6d31a1dde946daa370a9fc39540488ead
Mert <mertturkmenoglu99@gmail.com>
2019-06-16 00:36:08 +0300

    Add while loop example

---
+++ E013_Loop/main.py
@@ -0,0 +1,17 @@
+# Example 013: While loop example
+
+i = 0
+n = 5
+
+while i < n:
+    print(i)
+    i += 1
+
+i = 0
+n = 3
+
+while i < n:
+    print(i)
+    i += 1
+else:
+    print('While loop is over')
