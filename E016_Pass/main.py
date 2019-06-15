1b8e5d4452f7573cf631d3ea42ddf8272cf2782a
Mert <mertturkmenoglu99@gmail.com>
2019-06-16 00:46:29 +0300

    Add pass keyword example

---
+++ E016_Pass/main.py
@@ -0,0 +1,18 @@
+# Example 016: Pass keyword example
+
+# We will create a class later but we just want to define it
+# We use pass keyword
+class CreateLater:
+    pass
+
+# We will create a function but now, we just define it.
+# We use pass keyword
+def implementLater():
+    pass
+
+values = ['a', 'b', 'c', 'd']
+
+for char in values:
+    pass
+
+print('After for loop')
