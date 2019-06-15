3c999e37f9923359afe8fc2c5fc08ad5a130e703
Mert <mertturkmenoglu99@gmail.com>
2019-06-15 22:35:08 +0300

    Add special operators example

---
+++ E009_SpecialOperators/main.py
@@ -0,0 +1,16 @@
+# Example 009: Special operators example
+
+first_list = ['Robin', 'Galahad', 'Lancelot']
+second_list = ['Robin', 'Galahad', 'Lancelot']
+third_list = ['Galahad', 'Robin', 'Lancelot']
+
+print(first_list is second_list)
+print(first_list is third_list)
+print(first_list is not third_list)
+print('Robin' in first_list)
+print('King Arthur' not in first_list)
+
+
+name1 = 'Sherlock'
+name2 = 'Sherlock'
+print(name1 is name2)
