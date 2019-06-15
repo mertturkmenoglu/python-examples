f314991cf88b9f46e199bf8c7a029251a033e5bd
Mert <mertturkmenoglu99@gmail.com>
2019-06-15 21:48:29 +0300

    Add data types example

---
+++ E005_DataTypes/main.py
@@ -0,0 +1,41 @@
+# Example 005: Data types example
+
+answer = 42
+message = 'Ni!'
+pi = 3.14
+is_european_swallow = True
+very_big_number = 1000
+complex_number = 1 + 1j
+
+knights = ['Robin', 'Galahad', 'Lancelot', 'Sir Not Appearing In This Film']
+hold_data = [answer, pi, knights, True]
+
+my_tuple = ('Coconuts', 'Squirrel', 7, False)
+
+my_dictionary = {
+    'name': "King Arthur",
+    'age': 42,
+    'alive': True
+}
+
+print(answer)
+print(message)
+print(pi)
+print(is_european_swallow)
+print(very_big_number)
+print(complex_number)
+print(knights)
+print(hold_data)
+print(my_tuple)
+print(my_dictionary)
+
+print(type(answer))
+print(type(message))
+print(type(pi))
+print(type(is_european_swallow))
+print(type(very_big_number))
+print(type(complex_number))
+print(type(knights))
+print(type(hold_data))
+print(type(my_tuple))
+print(type(my_dictionary))
