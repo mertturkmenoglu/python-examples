f81f87a3264ff80c9ec538bcfc098d85f74dae07
Mert <mertturkmenoglu99@gmail.com>
2019-06-15 21:05:58 +0300

    Add basic variable example

---
+++ E002_Variable/main.py
@@ -0,0 +1,25 @@
+# Example 002: Basic variable declaration example
+
+num = 5 # integer
+my_string = "string variable"
+floating_num = 3.14
+boolean_var = True
+
+# Assign a value to multiple variables
+var1 = var2 = var3 = 10
+
+# Assign multiple values to multiple variables
+a1, a2, a3, a4, a5 = 1, 2, 3, 4, 5
+
+print(num)
+print(my_string)
+print(floating_num)
+print(boolean_var)
+print(var1)
+print(var2)
+print(var3)
+print(a1)
+print(a2)
+print(a3)
+print(a4)
+print(a5)
