5445e33076456c556a38a82c16853c03f342051d
Mert <mertturkmenoglu99@gmail.com>
2019-06-15 21:23:40 +0300

    Add dictionary example

---
+++ E004_Dictionary/main.py
@@ -0,0 +1,16 @@
+# Example 004: Dictionary
+# Key : Value pairs
+# Like hash-maps in Java
+
+print(dict.__doc__)
+print('\n\n\n')
+
+my_dictionary = {
+    'name': 'king arthur', 
+    'age': 42, 
+    'color':'blue'
+}
+
+print(my_dictionary)
+print(my_dictionary.keys())
+print(my_dictionary.values())
