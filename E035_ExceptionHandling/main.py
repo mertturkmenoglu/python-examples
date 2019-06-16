542569214fce918bba0bdd08e2681b3d9666931b
Mert <mertturkmenoglu99@gmail.com>
2019-06-16 23:10:08 +0300

    Add exception handling example

---
+++ E035_ExceptionHandling/main.py
@@ -0,0 +1,30 @@
+# Example 035: Exception Handling example
+import sys
+
+my_list = ['kadikoy', 3, 0, 'a',]
+
+for item in my_list:
+    try:
+        print("Item is: ", item)
+        result = 42 / item
+    except:
+        print("Exception: ", sys.exc_info()[0], " occured.")
+
+numbers = [1, 3, 5, 7, 9]
+
+try:
+    for number in numbers:
+        if number in my_list:
+            raise ValueError('Value is in the list')
+        else:
+            print('Value is not in the list, search continues')
+except ValueError as v_error:
+    print(v_error)
+
+
+try:
+    file = open('text.txt', mode = "r", encoding = "utf-8")
+    for line in file:
+        print(line)
+finally:
+    file.close()
