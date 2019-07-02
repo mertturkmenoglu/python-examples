d8b19fb857af55e10a90edbbe21f234eef1eaa44
Mert <mertturkmenoglu99@gmail.com>
2019-07-02 22:41:22 +0300

    Update variable example

--- examples/E002_Variable/main.py
+++ examples/E002_Variable/main.py
@@ -1,8 +1,15 @@
 # Example 002: Basic variable declaration example
 
-num = 5 # integer
+# integer
+num = 5 
+
+# string
 my_string = "string variable"
+
+# floating number
 floating_num = 3.14
+
+# boolean
 boolean_var = True
 
 # Assign a value to multiple variables
@@ -11,15 +18,16 @@ var1 = var2 = var3 = 10
 # Assign multiple values to multiple variables
 a1, a2, a3, a4, a5 = 1, 2, 3, 4, 5
 
-print(num)
-print(my_string)
-print(floating_num)
-print(boolean_var)
-print(var1)
-print(var2)
-print(var3)
-print(a1)
-print(a2)
-print(a3)
-print(a4)
-print(a5)
+# Print all values
+print("num: ", num)
+print("my_string: ", my_string)
+print("floating_num: ", floating_num)
+print("boolean_var: ", boolean_var)
+print("var1: ", var1)
+print("var2: ", var2)
+print("var3: ", var3)
+print("a1: ", a1)
+print("a2: ", a2)
+print("a3: ", a3)
+print("a4: ", a4)
+print("a5: ", a5)
