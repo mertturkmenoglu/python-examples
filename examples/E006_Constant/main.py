6f092eeea38b1ac37bd3d7aa119c04d1208317d6
Mert <mertturkmenoglu99@gmail.com>
2019-07-02 23:01:04 +0300

    Update constant example

--- examples/E006_Constant/main.py
+++ examples/E006_Constant/main.py
@@ -1,11 +1,16 @@
 # Example 006: Constant variable
 import constant
 
+# Because of design principles, we do not have constanst in Python
+# You may declare constant values in other languages via 'const' in C,
+# 'val' in Kotlin, 'final' in Java... etc. But in Python, we do not have this
+# We can declare variables(we call them variables but actually they do not change)
+# in modules. We can call this module as constant, for example. So it is clearer that 
+# the value in this module should not be changed
+
 print(constant.PI)
 print(constant.NAME)
 
 # However, you can change it...
-# We declare it in a module so it is more clear that
-# the value should not be changed
 constant.PI = 1
 print(constant.PI)
