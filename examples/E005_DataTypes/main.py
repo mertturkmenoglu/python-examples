a2be649a79eabd79c2cb976aa3ae769677c1361d
Mert <mertturkmenoglu99@gmail.com>
2019-07-02 22:57:40 +0300

    Update data types example

--- examples/E005_DataTypes/main.py
+++ examples/E005_DataTypes/main.py
@@ -1,5 +1,11 @@
 # Example 005: Data types example
 
+# Python supports different data types:
+# Integer
+# String (you may know them as char arrays)
+# Boolean (true or false)
+# Python2 supports long but after python3, integer and long are merged
+# Complex number from high school math(or university)
 answer = 42
 message = 'Ni!'
 pi = 3.14
@@ -7,17 +13,27 @@ is_european_swallow = True
 very_big_number = 1000
 complex_number = 1 + 1j
 
+# Lists. They are like arrays in other languages. 
+# But they can contain multiple type items
+# They are like dynamic containers
+# We represent them within '[' and ']' characters
 knights = ['Robin', 'Galahad', 'Lancelot', 'Sir Not Appearing In This Film']
 hold_data = [answer, pi, knights, True]
 
+# Tuples and lists are siblings. One big difference:
+# List are mutable(they can be changed) but tuples are
+# immutable(they can not be changed)
 my_tuple = ('Coconuts', 'Squirrel', 7, False)
 
+# Dictionaries. They are containers that holds values with unique keys
 my_dictionary = {
     'name': "King Arthur",
     'age': 42,
     'alive': True
 }
 
+# Lets print them and see how they behave
+# Print all the values and containers
 print(answer)
 print(message)
 print(pi)
@@ -29,6 +45,8 @@ print(hold_data)
 print(my_tuple)
 print(my_dictionary)
 
+# type() returns type of the object.
+# Let's see Pythonian way to represent data
 print(type(answer))
 print(type(message))
 print(type(pi))
