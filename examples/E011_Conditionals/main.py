19cbacb44c2009eb357a5f3d83f9df1299e79111
Mert <mertturkmenoglu99@gmail.com>
2019-07-03 22:19:18 +0300

    Update conditionals example

--- examples/E011_Conditionals/main.py
+++ examples/E011_Conditionals/main.py
@@ -1,12 +1,20 @@
 # Example 011: Conditional statements example
 
+# Create a variable
 age = 30
 
+# Check if the value is greater or equal to 18
+# If it satisfies the condition, then execute the indented code below
+# Otherwise, continue to normal program execution flow
 if age >= 18:
     print('You are an adult')
 
 characters = ['sherlock', 'watson', 'moriarty']
 
+# Check if element is in the list
+# If so, execute given code
+# Otherwise execute the code in the else branch.
+# Then continue to normal program execution flow
 if 'bond' in characters:
     print('It is in Sherlock Holmes series')
 else:
@@ -14,6 +22,7 @@ else:
 
 number = -2
 
+# Nested condition check. Similar approach.
 if number > 0:
     print('Number is positive')
 elif number < 0:
@@ -26,6 +35,8 @@ second_list = [3, 4, 5]
 
 number = 3
 
+# You can use elif keyword or you can use nested if else 
+# branching. This is a general approach but elif is the Python way.
 if number in first_list:
     if number in second_list:
         print('Number is in both of the lists')
