f403dec9bd4320a99d3bc062313296c75462a307
Mert <mertturkmenoglu99@gmail.com>
2019-07-03 22:08:59 +0300

    Update output formatting example

--- examples/E008_OutputFormatting/main.py
+++ examples/E008_OutputFormatting/main.py
@@ -1,13 +1,22 @@
 # Example 008: Output formatting
 
+# Create two dummy variable
 x = 3
 y = 4
 
+# This is the Python3 way.
+# You should use string format method.
 print('Point: {} - {}'.format(x, y))
-print('x: {x}\ty: {y}'.format(x = x,y = y))
+
+# You can specify variable name
+print('x: {x}\ty: {y}'.format(x=x, y=y))
+
+# Old, C style formatted output. Prefer not use to.
 print('%d - %d' %(x, y))
 
+# Take input
 first = int(input('Enter the first integer: '))
 second = int(input('Enter the second integer: '))
 
+# Print formatted
 print('You entered {} and {}'.format(first, second))
