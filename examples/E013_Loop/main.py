f95c8a75abba523d6178df802b61ea01bd7f1e2c
Mert <mertturkmenoglu99@gmail.com>
2019-07-05 23:42:44 +0300

    Update loop example

--- examples/E013_Loop/main.py
+++ examples/E013_Loop/main.py
@@ -1,5 +1,8 @@
 # Example 013: While loop example
 
+# Good old while loop, it works as you expected
+# Do not forget to change loop variable somewhere inside
+# your loop. And double check it.
 i = 0
 n = 5
 
@@ -10,8 +13,11 @@ while i < n:
 i = 0
 n = 3
 
+# You can have else branch on while loop. Just like in for loop example
 while i < n:
     print(i)
     i += 1
 else:
     print('While loop is over')
+
+print('After everything')
