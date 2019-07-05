1c58b4dd4b34699e1dfce1ce0e8285b04bfeb3dd
Mert <mertturkmenoglu99@gmail.com>
2019-07-05 23:46:34 +0300

    Update break keyword example

--- examples/E014_Break/main.py
+++ examples/E014_Break/main.py
@@ -1,7 +1,14 @@
 # Example 014: Break keyword example
 
+# You have break keyword in other languages. They are considered
+# as a bad programming habit. Do not use them if you really have to.
+# But in Python, sometimes it makes your life easier.
+
+# Lets have a list
 students = ['Emily', 'Jonathan', 'Bruce', 'Anna', 'Su']
 
+# Iterate over the list "UNTIL" you find the Bruce.
+# When you find the Bruce, "BREAK" the loop and continue
 for student in students:
     if student == 'Bruce':
         print('I found the Bruce')
@@ -18,3 +25,14 @@ while i < n:
         print('Break the loop')
         break
     i += 1
+
+i = 0
+n = 10
+# A bad example to use break.
+# Just use for loop.
+while True:
+    if i == n:
+        break
+    else:
+        print(i)
+        i += 1
