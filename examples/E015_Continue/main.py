c44a69c2ca4c96a6ea7123147c4d6b72f10b74ff
Mert <mertturkmenoglu99@gmail.com>
2019-07-05 23:47:41 +0300

    Update continue keyword example

--- examples/E015_Continue/main.py
+++ examples/E015_Continue/main.py
@@ -1,5 +1,9 @@
 # Example 015: Continue keyword example
 
+# It works as you expected
+# Like break, it is considered as a bad programming habit.
+# Do not use it unless you have to.
+
 # Print only odd numbers in range
 for i in range(0, 10):
     if i % 2 == 0:
