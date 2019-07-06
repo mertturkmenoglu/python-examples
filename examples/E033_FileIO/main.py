369aefa843be765467794b7f8d541dc76200db0d
Mert <mertturkmenoglu99@gmail.com>
2019-07-06 23:24:01 +0300

    Update file input example

--- examples/E033_FileIO/main.py
+++ examples/E033_FileIO/main.py
@@ -1,25 +1,34 @@
 # Example 033: File reading example
-# Note: In Visual Studio Code with Python extension(intellisense), line 21 gives an error
+# Note: In Visual Studio Code with Python extension(intellisense), print() call with 
+# named argument gives an error.
 # But it runs fine. I do not know what is the problem.
 
+# Open the file stream in classical way.
 file = open("input.txt", mode = "r", encoding = "utf-8")
 
-# Read first 10 data
+# Read first 8 data(bytes) and print it to stdout
 print(file.read(8))
-# Read more
+
+# Read more bytes
 print(file.read(6))
 
+# Where is the file indicator?
 print("file.tell(): ", file.tell())
+
+# Reset it
 file.seek(0)
 
-# Read all
+# Read all data in the file and print it to stdout
 print(file.read())
 
+# Close the file object
 file.close()
 
 # ------------- #
+# Best way to read a file line by line.
 file = open("input.txt", mode = "r", encoding = "utf-8")
 for line in file:
     print(line, end='')
+
 print()
-file.close
+file.close()
