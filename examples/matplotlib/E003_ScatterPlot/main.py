e9ff3abc268bea8454f72ca1ec383f1545e41b4d
mertturkmenoglu <mertturkmenoglu99@gmail.com>
2019-08-15 12:15:25 +0300

    Add matplotlib example

---
+++ examples/matplotlib/E003_ScatterPlot/main.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    x_values = [1, 1, 2, 3, 5, 8, 13]
+    y_values = [1, 1, 1, 1, 1, 1, 1]
+
+    plt.scatter(x_values, y_values)
+    plt.show()
